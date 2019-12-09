a430cf494aec72adcb635a71eee70e79268f9b76
Jakob Jan Kamminga <jakobjankamminga@gmail.com>
2019-12-09 21:42:24 +0100

    Add music section plus many small changes

--- themes/pulp/static/js/flower.js
+++ themes/pulp/static/js/flower.js
@@ -1,9 +1,7 @@
 const button = document.getElementById('radial-menu');
-const buttonIcon = document.getElementById('button-icon');
 const container = document.getElementById('nav');
 
 button.addEventListener('click', function(e){
     button.classList.toggle('is-active');
     container.classList.toggle('is-active');
-    buttonIcon.classList.toggle('fa-minus-circle');
 })
