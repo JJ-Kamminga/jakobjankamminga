bf331c3a78653bfb5fb7a47752a7124a9fd4a642
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-03-10 11:22:42 +0100

    Start over

--- src-archive/flower.ts
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('radial-menu');
-const container = document.getElementById('nav');
-
-button.addEventListener('click', function(e){
-    button.classList.toggle('is-active');
-    container.classList.toggle('is-active');
-})
