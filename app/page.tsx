c6a17a87ed8c7f2cbbb20f83bfe587ffc3dde29f
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-03-10 15:56:32 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,40 @@
-import { Container } from '@mui/material'
+import { Container, Divider, Typography } from '@mui/material'
 import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
     <section>
       <Container maxWidth='sm'>
-        <h1>
-          My Portfolio
-        </h1>
-        <p>
-          {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-        </p>
+        <Typography variant='h1'>Jakob Jan Kamminga</Typography>
+        {/* <Typography variant='subtitle1'>Quality engineer <i>by trade</i></Typography>
+        <Typography variant='subtitle1'>Music enthusiast <i>by heart</i></Typography>
+        <Typography variant='subtitle1'>Philosopher <i>whether I want it or not</i> (but also by MA degree)</Typography> */}
+        <Typography variant='body1' sx={{ padding: '10px' }} >
+          <Typography component='div' sx={{ backgroundColor: 'lightgrey' }}>If you've worked in software development,</Typography>
+          you probably know that everyone dreams of one thing: <i>to deliver value</i>.
+          <br />
+        </Typography>
+        <Typography variant='body1' sx={{ padding: '10px' }}>
+          <Typography component='div' sx={{ backgroundColor: 'lightgrey' }}>
+            If you've studied philosophy,
+          </Typography>
+          you probably know where this is going.
+          <br />
+        </Typography>
+        <Typography variant='body1' sx={{ padding: '10px' }} >
+          I help teams build better software.
+          <div>
+            80% asking questions.
+          </div>
+          <Divider />
+          <div>
+            20% writing code and CI/CD pipelines.
+          </div>
+        </Typography>
         <div>
           <BlogPosts />
         </div>
       </Container>
-    </section>
+    </section >
   )
 }
