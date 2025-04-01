0656a3ea4eab1ec3ec9ec5a8180c327ad7e87716
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-04-01 20:18:57 +0200

    config

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,17 +2,20 @@ import { ImageResponse } from 'next/og';
 
 export function GET(request: Request) {
   const url = new URL(request.url);
-  const title = url.searchParams.get('title') || 'Next.js Portfolio Starter';
+  const title = url.searchParams.get('title') || 'Jakob Jan Kamminga';
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
-          </h2>
-        </div>
-      </div>
+      <h2>
+        {title}
+      </h2>
+      // <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+      //   <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+      //     <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+      //       {title}
+      //     </h2>
+      //   </div>
+      // </div>
     ),
     {
       width: 1200,
