086a2c0d65043c07527fc661c388c2607e4b900a
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-08-29 17:00:48 +0200

    solve hydration issue on blog

--- app/components/postcontents.tsx
+++ app/components/postcontents.tsx
@@ -1,10 +1,10 @@
-"use client";
-
 import { Typography } from "@mui/material";
-
 export const PostContents = ({ contents }: { contents: string }) => {
   return (
-    <Typography variant='body1' sx={{ fontSize: '1.2rem' }} dangerouslySetInnerHTML={{ __html: contents }}>
-    </Typography>
+    <Typography
+      variant='body1'
+      sx={{ fontSize: '1.2rem' }}
+      dangerouslySetInnerHTML={contents ? { __html: contents } : undefined}
+    />
   );
 };
